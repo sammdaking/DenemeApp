eb447e0bbb0afb9295e97d248191f07e01139596
sammdaking <sammdaking7@gmail.com>
2023-07-06 17:03:44 +0300

    third commit

--- app/src/main/java/com/example/denemeapp/GiveFragment.java
+++ app/src/main/java/com/example/denemeapp/GiveFragment.java
@@ -1,12 +1,37 @@
 package com.example.denemeapp;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultCaller;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.denemeapp.databinding.FragmentGiveBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+import javax.xml.transform.Result;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +40,10 @@ import android.view.ViewGroup;
  */
 public class GiveFragment extends Fragment {
 
+    FragmentGiveBinding binding;
+    Uri imageData;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -46,8 +75,13 @@ public class GiveFragment extends Fragment {
         return fragment;
     }
 
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        registerLauncher();
+
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
@@ -55,10 +89,106 @@ public class GiveFragment extends Fragment {
         }
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        binding = FragmentGiveBinding.inflate(inflater,container,false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_give, container, false);
+        View view =  inflater.inflate(R.layout.fragment_give, container, false);
+
+        return binding.getRoot();
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button button = view.findViewById(R.id.button);
+        ImageView imageView = view.findViewById(R.id.setImageView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setData(view);
+            }
+        });
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setImage(view);
+            }
+        });
+    }
+
+
+
+
+
+ // push tuşuna basınca firebase e verileri kaydeden fonksiyon
+    public void setData(View view){
+
+    }
+
+    private void registerLauncher(){
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK)
+                    {
+                        Intent intentFromResult = result.getData();
+                        if(intentFromResult != null)
+                        {
+                             imageData =intentFromResult.getData();
+                             binding.setImageView.setImageURI(imageData); // First method
+
+                        }
+                    }
+            }
+        });
+
+
+
+
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                 if(result){
+                    Intent intentToGalery = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    activityResultLauncher.launch(intentToGalery);
+                 }
+                 else {
+                     // get context i chech et
+                     Toast.makeText(getContext(),"Permission Needed", Toast.LENGTH_SHORT).show();
+                 }
+            }
+        });
+    }
+
+     // Imageview a basınca galeriden fotoğraf almayı sağlayan izin kodları"
+    public  void setImage (View view){
+    // buraya dikkat
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_DENIED)
+        {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this.getActivity(),Manifest.permission.READ_EXTERNAL_STORAGE))
+            {
+                Snackbar.make(view,"Permision needed galeri",Snackbar.LENGTH_INDEFINITE).setAction("Give permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // ask permission
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    }
+                }).show();
+            }       else {// ask permission
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+
+        } else {
+            Intent intentToGalery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            activityResultLauncher.launch(intentToGalery);
+
+        }
+
     }
 }
